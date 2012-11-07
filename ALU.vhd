9e55bcbcc598ed862d65775835290b87b30e8602
npbool <iwnking@gmail.com>
2012-11-07 21:39:13 +0800

    alu modify

--- ALU.vhd
+++ ALU.vhd
@@ -4,27 +4,80 @@ use IEEE.STD_LOGIC_UNSIGNED.ALL;
 use IEEE.STD_LOGIC_ARITH.ALL;
 
 entity ALU is
-    Port ( A,B : in STD_LOGIC_VECTOR(31 downto 0);           
-			  OP	: in STD_LOGIC_VECTOR(3 downto 0);
-			  ALU_OUT : out STD_LOGIC_VECTOR(31 downto 0);
-           ALU_ZERO : out STD_LOGIC);	 
+    Port ( A,B : in STD_LOGIC_VECTOR(31 downto 0);
+			  Code: in STD_LOGIC_VECTOR(3 downto 0);
+			  Func: in STD_LOGIC_VECTOR(5 downto 0);
+			  R : out STD_LOGIC_VECTOR(31 downto 0);
+           Zero : out STD_LOGIC;
+			  Sign : out STD_LOGIC);	 
 end ALU;
 architecture Behavioral of ALU is
+	signal OP : STD_LOGIC_VECTOR(3 downto 0);
 	signal result : STD_LOGIC_VECTOR(31 downto 0);
-begin
+begin	
+	PROCESS(Code,Func,A,B)
+	begin
+	
+	case Code is
+	when "0000" =>		
+		with Func select
+			OP <= "0000" when 33, --add
+					"0010" when 42, --slt
+					"0011" when 43, --sltu
+					"0001" when 35, --sub
+					"0100" when 36, --and
+					"0101" when 39, --nor
+					"0111" when 37, --or
+					"0110" when 38, --xor
+					"1000" when 0 | 4, --sll
+					"1001" when 3 | 7, --sra
+					"1010" when 2 | 6, --srl
+					"1111" when others;
+					
+	when "1001" =>
+		OP<="0000";
+	when "1010" =>
+		OP<="0010";
+	when "1011" =>
+		OP<="0011";
+	when "0100" | "0001" | "0111" | "0110" | "0001" | "0101"=>
+		OP<="0001";
+	when "1100" =>
+		OP<="0100";
+	when "1101" =>
+		OP<="0111";
+	when "1110" =>
+		OP<="0110";
+	when others =>
+		OP<="1111";						
+	end case;
+	
+	
 	case OP is
 	when "0000" =>
 		result<=A+B;	
 	when "0001" =>
 		result<=A-B;
+	when "0010" => --slt
+		if CONV_INTEGER(A)<CONV_INTEGER(B) then
+			result<=1;
+		else
+			result<=0;
+		end if;
+	when "0011" => --sltu
+		if CONV_UNSIGNED(A)<CONV_UNSIGNED(B) then
+			result<=1;
+		else
+			result<=0;
+		end if;
 	when "0100" =>
 		result<=A and B;
 	when "0101" =>
 		result<=A nor B;
 	when "0110" =>
-		result<=A or B;
+		result<=A xor B;
 	when "0111" =>
-		result<=not A;					
+		result<=A or B;					
 	when "1000" =>					
 		result<=TO_STDLOGICVECTOR(TO_BITVECTOR(A) sll CONV_INTEGER(B));
 	when "1001" =>					
@@ -35,9 +88,12 @@ begin
 		result <= A;
 	end case;
 	if(result=0) then
-		ALU_ZERO<='1';
+		Zero<='1';
 	else
-		ALU_ZERO<='0';
+		Zero<='0';
 	end if;
-	ALU_OUT <= result;
+	Sign<=result(31);
+	R <= result;
+	
+	end PROCESS;
 end Behavioral;
--- Mem.vhd
+++ Mem.vhd
@@ -32,10 +32,13 @@ use IEEE.STD_LOGIC_1164.ALL;
 entity Mem is
 PORT(
 		Addr	:IN STD_LOGIC_VECTOR(31 downto 0);
-		Data	:INOUT STD_LOGIC_VECTOR(31 downto 0);
+		Data	:INOUT STD_LOGIC_VECTOR(31 downto 0); --高阻？？
 		En,Rw	:IN STD_LOGIC;
 		Done	:OUT STD_LOGIC;
+		
 		--TLB:TODO
+		Tlb_missing	:OUT STD_LOGIC;
+		
 		clk,rst:IN STD_LOGIC;
 		
 		--管脚，顶层引入
@@ -51,14 +54,110 @@ PORT(
 		Flash_data:INOUT STD_LOGIC_VECTOR(15 downto 0);
 		Flash_byte,Flash_ce,Flash_ce1,Flash_ce2,
 		Flash_oe,Flash_rp,Flash_sts,Flash_vpen,Flash_we:OUT STD_LOGIC
+		--串口切换：rw,en,oe全1，rdn,wrn=1（InterConn3,6)
 	);
 end Mem;
 
 architecture Behavioral of Mem is
 
-PROCESS(
+signal working:STD_LOGIC;
+signal count:STD_LOGIC_VECTOR(1 downto 0);
+signal op:STD_LOGIC_VECTOR(2 downto 0); 
+--000:RamRead,001:RamWrite
+--010:serial read,011:w
+--100:flash read,101:f w
+
+--1FD003F8:串口数据。
+--1FD003FC:串口状态。S&1=1可写，S&2=2可读。
 
 begin
+	working <= '0';
 
+	Ram2_oe<='0';
+	Ram2_oe<='0';
+	Flash_ce<='0';
+	Flash_ce1<='0';
+	Flash_ce2<='0';		
+	
+	PROCESS(clk)
+	begin		
+		if working='0' and En='1' then
+			working<='1';
+			Done<='0';
+			if Addr=x"1FD003F8" then
+				--serial
+				Ram1_en<='1';
+				Ram1_oe<='1';
+				Ram1_rw<='1';
+				if Rw='0' then
+					--read serial
+					
+				else
+					--write serial
+				end if;				
+			elsif Addr=x"1FD003FC" then
+				Data<="ZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZ";
+				Data<=x"00000000" or (Tbre and Tsre) or (Data_ready sll 1);
+				Done<=1;
+			elsif Rw='0' then
+				--read Ram
+				Ram1_addr<=Addr(17 downto 0);
+				Ram1_data<="ZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZ";
+				Ram1_rw<='1';
+				Ram2_addr<=Addr(17 downto 0);				
+				Ram2_data<="ZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZ";
+				Ram2_rw<='1';				
+				op<="00";
+			else
+				--write
+				Ram1_addr<=Addr(17 downto 0);		
+				Ram1_data<=Data;
+				Ram1_rw<='0';
+				Ram2_addr<=Addr(17 downto 0);				
+				Ram2_data<=Data;
+				Ram2_rw<='0';							
+			end if;			
+		elsif working='1' then
+			case op is
+			when "00" => --prev ram read;
+				case count is
+				when "00" =>
+					Data<="ZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZ";
+					Done<='1';
+					Data(31 downto 16)<=Ram1_data;
+					Data(15 downto 0)<=Ram2_data;
+					working<='0';
+				when "01" =>
+					count<="00";
+				when "10" =>
+					count<="01";
+				when "11" =>
+					count<="10";
+				when others =>
+					null;
+				end case;
+			when "01" => --prev ram write				
+				case count is
+				when "00" =>
+					Done<='1';					
+					working<='0';
+				when "01" =>
+					count<="00";
+				when "10" =>
+					count<="01";
+				when "11" =>
+					count<="10";
+				when others =>
+					null;
+				end case;
+			when "10" => --prev serial write
+				null;
+			when "11" => --prev flash op
+				null;
+			when others=>
+				null;
+			end case;
+		end if;
+	end PROCESS;	
 end Behavioral;
 
